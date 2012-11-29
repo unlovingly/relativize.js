be2ee158b969488818928d87299d41f714c1638a
l6y <mail.address.is.unsteady@gmail.com>
2012-11-29 17:01:06 +0900

    Added relativize.js

---
+++ relativize.js
@@ -0,0 +1,103 @@
+(function () {
+  'use strict';
+
+  var conversions, identifier, locale, options, units;
+
+  conversions = [
+    1000,
+    60,
+    60,
+    24,
+    30,
+    12,
+    100,
+  ];
+  identifier = {
+    en: {
+      past: ' ago',
+    },
+    ja: {
+      past: '前',
+    },
+  },
+  locale = {
+    en: {
+      pluralable: true,
+    },
+    ja: {
+      pluralable: false,
+    },
+  },
+  options = {
+    locale: 'en',
+  };
+  units = {
+    en: [
+      'second',
+      'minute',
+      'hour',
+      'day',
+      'month',
+      'year',
+    ],
+    ja: [
+      '秒',
+      '分',
+      '時間',
+      '日',
+      '月',
+      '年',
+    ],
+  };
+
+  function _pluralize(target) {
+    var targets;
+
+    if (!locale[options.locale].pluralable) return target;
+
+    targets = target.split(' ');
+
+    return targets[0] + ' ' + ((parseInt(targets[0]) > 1) ? targets[1] + 's' : targets[1]);
+  }
+
+  function relativize(params) {
+    var delta;
+
+    if (params !== undefined) for (var i = 0, k = Object.keys(params), l = k.length; i < l; i++) {
+      options[k[i]] = params[k[i]];
+    }
+
+    delta = new Date() - this;
+
+    for (var i = 0, l = conversions.length; i < l; i++) {
+      delta = Math.floor(delta / conversions[i]);
+
+      if (delta < conversions[i + 1]) {
+        return _pluralize(delta + ' ' + units[options.locale][i], options.locale)
+            + identifier[options.locale]['past'];
+      }
+    }
+  }
+
+  function toDate() {
+    return new Date(Date.parse(this));
+  }
+
+  Object.defineProperties(Date.prototype, {
+    relativize: {
+      configurable: true,
+      enumerable: false,
+      writable: true,
+      value: relativize,
+    },
+  });
+
+  Object.defineProperties(String.prototype, {
+    toDate: {
+      configurable: true,
+      enumerable: false,
+      writable: true,
+      value: toDate,
+    },
+  });
+})();
